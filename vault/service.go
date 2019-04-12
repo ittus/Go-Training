7e90dfc7ae57e7c96c70fa50e49037741ea8c3c6
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-04-12 18:01:35 +0900

    Add protobuff and service

---
+++ vault/service.go
@@ -0,0 +1,35 @@
+package vault
+
+import (
+	"context"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// Service provides password hashing capabilities.
+type Service interface {
+	Hash(ctx context.Context, password string) (string, error)
+	Validate(ctx context.Context, password, hash string) (bool, error)
+}
+
+func NewService() Service {
+	return vaultService{}
+}
+
+type vaultService struct{}
+
+func (vaultService) Hash(ctx context.Context, password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func (vaultService) Validate(ctx context.Context, password, hash string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false, nil
+	}
+	return true, nil
+}
